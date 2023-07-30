6e009524c1223e536d98de0ab593b8b5f54b5ee0
SlimRunner <dv.polygon@gmail.com>
2023-07-30 15:59:20 -0700

    documentation update

--- public/data-collector.js
+++ public/data-collector.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict";
-  const HIST_SIZE = 50;
+  const HIST_SIZE = 200;
   let dataSet = {};
 
   function updateWidget(canvas, id) {
@@ -15,38 +15,31 @@
     const xscale = WIDTH / HIST_SIZE;
 
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    if (id === "humidity" || id === "temperature") {
-      const datum = dataSet[id][dataSet[id].length - 1];
-      const PI4 = Math.PI / 4;
-      let t = (datum - ymin) / (ymax - ymin);
-      t = (1 - t) * 5 * PI4 - t * PI4;
-      const r = WIDTH * 0.8;
-      const xm = WIDTH / 2;
-      const ym = HEIGHT / 2;
-      ctx.beginPath();
-      ctx.moveTo(xm, ym);
-      console.log(xm, ym);
-      console.log(r * Math.cos(t), r * Math.sin(t));
-      ctx.lineTo(xm + r * Math.cos(t), ym - r * Math.sin(t));
-      ctx.stroke();
-    } else {
-      ctx.beginPath();
-      let xi = 0;
-      let first = true;
-      for (const datum of dataSet[id]) {
-        let x = xscale * xi;
-        let y = HEIGHT * (1 - (datum - ymin) / (ymax - ymin));
-        console.log(x,y);
-        if (first) {
-          ctx.moveTo(x, y);
-          first = false;
-        } else {
-          ctx.lineTo(x, y);
-        }
-        ++xi;
-      }
-      ctx.stroke();
+    ctx.beginPath();
+    let xi = 0;
+    let x, y;
+    ctx.moveTo(0, HEIGHT + 1);
+    for (const datum of dataSet[id]) {
+      x = xscale * xi;
+      y = HEIGHT * (1 - (datum - ymin) / (ymax - ymin));
+      ctx.lineTo(x, y);
+      ++xi;
     }
+    ctx.lineTo(x, HEIGHT + 1);
+    ctx.closePath();
+
+    if (id == "humidity") {
+      // ctx.fill
+    } else if (id == "temperature") {
+
+    } else if (id == "gas") {
+
+    } else if (id == "ultrasonic") {
+
+    }
+
+    ctx.stroke();
+    ctx.fill();
   }
 
   ["humidity", "temperature", "gas", "ultrasonic"].map((tag) => {
